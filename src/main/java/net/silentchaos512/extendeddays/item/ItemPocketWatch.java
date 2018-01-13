f68827009c3bfe8b5b47d9086c3b46820e3f63e0
SilentChaos512 <sworkman256@gmail.com>
2018-01-13 16:04:24 -0600

    Pocket watch recipes (#2)

--- src/main/java/net/silentchaos512/extendeddays/item/ItemPocketWatch.java
+++ src/main/java/net/silentchaos512/extendeddays/item/ItemPocketWatch.java
@@ -1,11 +1,13 @@
 package net.silentchaos512.extendeddays.item;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import net.silentchaos512.extendeddays.ExtendedDays;
 import net.silentchaos512.extendeddays.event.ClientEvents;
 import net.silentchaos512.lib.item.ItemSL;
+import net.silentchaos512.lib.registry.RecipeMaker;
 
 public class ItemPocketWatch extends ItemSL {
 
@@ -17,10 +19,17 @@ public class ItemPocketWatch extends ItemSL {
   }
 
   @Override
-  public void onUpdate(ItemStack stack, World world, Entity entity, int itemSlot, boolean isSelected) {
+  public void onUpdate(ItemStack stack, World world, Entity entity, int itemSlot,
+      boolean isSelected) {
 
     if (world.isRemote) {
       ClientEvents.playerHasPocketWatch = true;
     }
+  }
+
+  @Override
+  public void addRecipes(RecipeMaker recipes) {
+
+    recipes.addShapelessOre(NAME, new ItemStack(this), new ItemStack(Items.CLOCK), "gemQuartz");
   }
 }
